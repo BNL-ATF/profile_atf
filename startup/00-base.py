6fb9510b76865c3ca2935937e4fc72b76693f4de
Maksim Rakitin <mrakitin@bnl.gov>
2022-10-06 14:59:17 -0400

    Sync changes with the private repo (the latter one will be archived now)

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,19 +1,23 @@
 print(f"Loading {__file__}")
 
-from databroker.v0 import Broker
+import bluesky.plan_stubs as bps
+import bluesky.plans as bp
+import databroker
+import matplotlib.pyplot as plt
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
-
-import bluesky.plans as bp
-import bluesky.plan_stubs as bps
-
-import matplotlib.pyplot as plt
+from databroker.v0 import Broker
 
 # Enable interactive mode
 plt.ion()
 
 # db = Broker.named("temp")
 db = Broker.named("local")
+try:
+    databroker.assets.utils.install_sentinels(db.reg.config, version=1)
+except Exception:
+    pass
+
 
 RE = RunEngine({})
 RE.subscribe(db.insert)
---
+++ startup/20-madx.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+
+from sirepo_bluesky.sirepo_bluesky import SirepoBluesky
+from sirepo_bluesky.sirepo_ophyd import create_classes
+from sirepo_bluesky.madx_flyer import MADXFlyer
+from sirepo_bluesky.madx_handler import MADXFileHandler
+
+from ophyd.utils import make_dir_tree
+
+db.reg.register_handler("madx", MADXFileHandler, overwrite=True)
+
+root_dir = "/tmp/sirepo_flyer_data"
+_ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)
+
+ATF_SIREPO_URL = os.getenv("ATF_SIREPO_URL")
+if ATF_SIREPO_URL in (None, ""):
+    print("ATF_SIREPO_URL environment variable should be set to use Sirepo.")
+else:
+    print(f"Using Sirepo at {ATF_SIREPO_URL}")
+    connection = SirepoBluesky(ATF_SIREPO_URL)
+    data, schema = connection.auth("madx", "00000001")
+
+    classes, objects = create_classes(connection.data,
+                                      connection=connection)
+    globals().update(**objects)
+
+    madx_flyer = MADXFlyer(connection=connection,
+                           root_dir="/tmp/sirepo_flyer_data/",
+                           report="elementAnimation250-20")
+
+# RE(bp.fly([madx_flyer]))
+#
+# hdr = db["2c8504e4-8ac9-4e56-ab42-da5e3cb9b4e0"]
+# hdr.table(stream_name="madx_flyer", fill=True)
+#
+# def madx_plan():
+#     yield from bps.mv(fq1.k1, "ifq1/5.4444e-3/hr")
+#     yield from bp.fly([madx_flyer])
+#
+# RE(madx_plan())
+#
+# hdr2 = db["a3ed4aea-068e-4b15-85f7-3abdcf5cccc2"]
+#
+# hdr.table(stream_name="madx_flyer", fill=True).loc[21]
+# hdr2.table(stream_name="madx_flyer", fill=True).loc[21]
