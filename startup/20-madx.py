3f8142c4c3428a9e4b30e232904d19bf6e001a18
Maksim Rakitin <mrakitin@bnl.gov>
2022-10-12 17:53:28 -0400

    Add `madx_plan` with the docstring on how to use it

--- startup/20-madx.py
+++ startup/20-madx.py
@@ -32,18 +32,24 @@ else:
         report="elementAnimation250-20",
     )
 
-# RE(bp.fly([madx_flyer]))
-#
-# hdr = db["2c8504e4-8ac9-4e56-ab42-da5e3cb9b4e0"]
-# hdr.table(stream_name="madx_flyer", fill=True)
-#
-# def madx_plan():
-#     yield from bps.mv(fq1.k1, "ifq1/5.4444e-3/hr")
-#     yield from bp.fly([madx_flyer])
-#
-# RE(madx_plan())
-#
-# hdr2 = db["a3ed4aea-068e-4b15-85f7-3abdcf5cccc2"]
-#
-# hdr.table(stream_name="madx_flyer", fill=True).loc[21]
-# hdr2.table(stream_name="madx_flyer", fill=True).loc[21]
+
+    def madx_plan(parameter="ihq1", value=2.0):
+        """A bluesky plan to use MAD-X simulation package within Sirepo (via sirepo-bluesky lib).
+
+        Run:
+
+            uid, = RE(bp.fly([madx_flyer]))
+
+        Data access:
+
+            hdr = db[uid]
+            tbl = hdr.table(stream_name="madx_flyer", fill=True)
+
+        Plotting:
+
+            plt.plot(tbl['madx_flyer_S'], tbl['madx_flyer_BETX'])
+            plt.plot(tbl['madx_flyer_S'], tbl['madx_flyer_BETY'])
+
+        """
+        yield from bps.mv(objects_var[parameter].value, value)
+        return (yield from bp.fly([madx_flyer]))
