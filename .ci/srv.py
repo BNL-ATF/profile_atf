b449c59dae7b15cef3499b26742899abb29a8028
Maksim Rakitin <mrakitin@bnl.gov>
2022-12-13 23:36:55 -0500

    CI: parametrize receive/send buffer sizes via env vars

--- .ci/srv.py
+++ .ci/srv.py
@@ -9,6 +9,9 @@ def server_program(seed=0):
     host = os.getenv("ATF_SOCKET_HOST", "localhost")
     port = int(os.getenv("ATF_SOCKET_PORT", 5000))  # initiate port no above 1024
 
+    recv_buffer_size = int(os.getenv(ATF_DB_RECEIVE_BUFFER_SIZE, 5120))
+    send_buffer_size = int(os.getenv(ATF_DB_SEND_BUFFER_SIZE, 5120))
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # look closely. The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
@@ -24,7 +27,7 @@ def server_program(seed=0):
 
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        data = conn.recv(recv_buffer_size).decode()
         if not data:
             # if data is not received break
             break
